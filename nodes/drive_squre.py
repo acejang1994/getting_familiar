20e49a693b96bbe2ae51d7fca3417bdc25968c1c
acejang1994 <acejang1994@gmail.com>
2015-09-13 23:05:42 -0400

    adding learning_tf listenrs talker and drive_square

---
+++ nodes/drive_squre.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+ROS code to make the Neato drive in square 
+"""
+from geometry_msgs.msg import Twist
+import rospy
+
+# we have to initialize ourselves as a roscore
+rospy.init_node('drive_square')
+pub = rospy.Publisher("/square", Twist, queue_size=1)
+twist = Twist()
+
+def go():
+	print "in go"    
+	twist.linear.y = .1    
+	pub.publish(twist)    
+	rospy.sleep(10)     
+ 
+
+def turn():
+	print "in turn"
+	twist.angular.x = 1    
+	pub.publish(twist)    
+	rospy.sleep(1.5); 	
+
+while not rospy.is_shutdown():     
+	go()
+	turn()  
--- scripts/forward_send_sphere.py
+++ scripts/forward_send_sphere.py
@@ -11,13 +11,14 @@ import rospy
 # we have to initialize ourselves as a roscore
 rospy.init_node('push_message')
 
-header_msg = Header(stamp=rospy.Time.now(), frame_id="odom")
 marker = Marker()
+marker.header.frame_id = "base_link"
+
 marker.action = marker.ADD
 marker.type = marker.SPHERE
-marker.pose.position.x = 1.0
-marker.pose.position.y = 2.0
-marker.pose.position.z = 0.0
+marker.pose.position.x = 1
+marker.pose.position.y = 0
+marker.pose.position.z = 0
 marker.pose.orientation.w = 1.0
 marker.scale.x = .3
 marker.scale.y = .3
@@ -26,7 +27,7 @@ marker.color.a = 1.0
 marker.color.r = 1.0
 marker.color.g = 1.0
 marker.color.b = 1.0
-marker.header = header_msg
+
 
 pub = rospy.Publisher("/our_sphere", Marker, queue_size=10)
 
