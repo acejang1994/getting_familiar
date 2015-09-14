9e5c7139124c6fa1670a23ad9dce217c523d06d8
Filippos Lymperopoulos <filippos.lymperopoulos@students.olin.edu>
2015-09-13 23:32:21 -0400

    ADD: teleop file

---
+++ nodes/teleop.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""
+ROS Code for publishing topic where Neato robot moves in space
+"""
+
+from geometry_msgs.msg import Twist, Vector3
+
+import rospy
+import tty
+import select
+import sys
+import termios
+
+settings = termios.tcgetattr(sys.stdin)
+
+# Gets ascii value of keyboard hits
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    select.select([sys.stdin], [], [], 0)
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+# Initiate node and topic, as well as time rate
+pub = rospy.Publisher('/communication', Twist, queue_size=10)
+rospy.init_node('teleop_tranport')
+r = rospy.Rate(10)
+key = None
+
+# Loop for operation
+while not rospy.is_shutdown():
+	key = getKey()
+	linObj = Vector3()
+	angObj = Vector3()
+
+	# Key-based transformation
+	if key == 'w':
+	    linObj.x = 0.5
+	elif key == 'a':
+	    angObj.z = 1.0
+	elif key == 's':
+	    linObj.x = -0.5
+	elif key == 'd':
+	    angObj.z = -1.0
+
+	# Publishes Twist Object and waits
+	disp = Twist(linObj, angObj)
+	pub.publish(disp)
+	r.sleep()
+
+	if (key == '\x03'):
+	    break
